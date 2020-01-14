4ea343f0131b5ef120498d6bd54c13da309bae04
aandr <aandr@DESKTOP-FLECQH5>
2020-01-15 01:12:27 +0200

    regex2

---
+++ Regex2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Regex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"^((([0-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.)|(0x[0-9A-Fa-f][0-9A-Fa-f]\.)){3}((([0-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5]))|(0x[0-9A-Fa-f][0-9A-Fa-f]))$";
+            while (true)
+            {
+                Console.WriteLine("Enter IP");
+                string date = Console.ReadLine();
+
+                if (Regex.IsMatch(date, pattern))
+                {
+                    Console.WriteLine("yes");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("no");
+                }
+            }
+        }
+    }
+}
